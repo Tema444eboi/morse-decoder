c12e561557665a07f8e32888fe57a11682f5d2a1
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2023-09-19 17:33:10 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,76 @@
-const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+const MORSE_CODE = {  
+    "-----":"0",
+    ".----":"1",
+    "..---":"2",
+    "...--":"3",
+    "....-":"4",
+    ".....":"5",
+    "-....":"6",
+    "--...":"7",
+    "---..":"8",
+    "----.":"9",
+    ".-":"A",
+    "-...":"B",
+    "-.-.":"C",
+    "-..":"D",
+    ".":"E",
+    "..-.":"F",
+    "--.":"G",
+    "....":"H",
+    "..":"I",
+    ".---":"J",
+    "-.-":"K",
+    ".-..":"L",
+    "--":"M",
+    "-.":"N",
+    "---":"O",
+    ".--.":"P",
+    "--.-":"Q",
+    ".-.":"R",
+    "...":"S",
+    "-":"T",
+    "..-":"U",
+    "...-":"V",
+    ".--":"W",
+    "-..-":"X",
+    "-.--":"Y",
+    "--..":"Z",
+    "-.-.--":"!",
+    ".-.-.-":".",
+    "--..--":","
 };
 
-function decode(expr) {
-    // write your solution here
+decodeSymbol = function(morse){
+    return MORSE_CODE[morse]
 }
 
-module.exports = {
-    decode
+decodeMorse = function(morseCode){
+    res = ''
+
+    words =  morseCode.trim().split("   ")
+    // You can use MORSE_CODE[morse]
+    symbols = []
+    for (i = 0;i < words.length; i++){
+        symbols[i] = words[i].split(" ")
+    }
+
+    for(i= 0; i< symbols.length;i++){
+        if (i>0) res += " "
+        for(j = 0; j< symbols[i].length;j++){
+            res += decodeSymbol(symbols[i][j])
+        }
+        
+    }
+    return(res)
+  }
+//.... . -.--   .--- ..- -.. .
+
+input = document.getElementById("input")
+output = document.getElementById("output")
+
+output.defaultValue = "result"
+input.defaultValue = "input morse"
+
+input.oninput = function(){
+    output.value = decodeMorse(input.value)
 }

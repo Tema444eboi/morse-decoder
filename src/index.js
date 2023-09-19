316d2a9b4a71c920b23f8bc0bee276dc7a5c2bfa
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2023-09-19 17:36:46 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,76 +1,70 @@
-const MORSE_CODE = {  
-    "-----":"0",
-    ".----":"1",
-    "..---":"2",
-    "...--":"3",
-    "....-":"4",
-    ".....":"5",
-    "-....":"6",
-    "--...":"7",
-    "---..":"8",
-    "----.":"9",
-    ".-":"A",
-    "-...":"B",
-    "-.-.":"C",
-    "-..":"D",
-    ".":"E",
-    "..-.":"F",
-    "--.":"G",
-    "....":"H",
-    "..":"I",
-    ".---":"J",
-    "-.-":"K",
-    ".-..":"L",
-    "--":"M",
-    "-.":"N",
-    "---":"O",
-    ".--.":"P",
-    "--.-":"Q",
-    ".-.":"R",
-    "...":"S",
-    "-":"T",
-    "..-":"U",
-    "...-":"V",
-    ".--":"W",
-    "-..-":"X",
-    "-.--":"Y",
-    "--..":"Z",
-    "-.-.--":"!",
-    ".-.-.-":".",
-    "--..--":","
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
 };
 
-decodeSymbol = function(morse){
-    return MORSE_CODE[morse]
-}
+function decode(expr) {
+    let arr = expr.split('**********');
 
-decodeMorse = function(morseCode){
-    res = ''
+    let word = arr.map(function(decode) {
+        const simbols = decode.length / 10;
+        let decodeMessage = '';
 
-    words =  morseCode.trim().split("   ")
-    // You can use MORSE_CODE[morse]
-    symbols = []
-    for (i = 0;i < words.length; i++){
-        symbols[i] = words[i].split(" ")
-    }
+        for (let i = 0; i < simbols; i++) {
+            let word = '';
+            for (let j = i * 10; j < i * 10 + 10; j += 2) {
+                if (decode[j] + decode[j + 1] === '10') {
+                    word += '.';
+                } else if (decode[j] + decode[j + 1] === '11') {
+                    word += '-';
+                }
+            }
 
-    for(i= 0; i< symbols.length;i++){
-        if (i>0) res += " "
-        for(j = 0; j< symbols[i].length;j++){
-            res += decodeSymbol(symbols[i][j])
+            decodeMessage += MORSE_TABLE[word];
+            word = '';
         }
-        
-    }
-    return(res)
-  }
-//.... . -.--   .--- ..- -.. .
+        return decodeMessage;
+    });
 
-input = document.getElementById("input")
-output = document.getElementById("output")
+    let result = word.join(' ');
 
-output.defaultValue = "result"
-input.defaultValue = "input morse"
-
-input.oninput = function(){
-    output.value = decodeMorse(input.value)
+    return result;
 }
+
+module.exports = {
+    decode
+};
